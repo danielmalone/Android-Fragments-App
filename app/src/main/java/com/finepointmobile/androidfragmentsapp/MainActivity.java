6346aab71370ff1e8b61eef1e335378aa76c3955
Daniel Malone <danieljmalone@gmail.com>
2017-07-29 18:35:43 -0500

    make fragment

--- app/src/main/java/com/finepointmobile/androidfragmentsapp/MainActivity.java
+++ app/src/main/java/com/finepointmobile/androidfragmentsapp/MainActivity.java
@@ -1,11 +1,19 @@
 package com.finepointmobile.androidfragmentsapp;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    FloatingActionButton mFab;
+    MainFragment mainFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,9 +21,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mainFragment = new MainFragment();
 
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeFragment();
+                Log.d(TAG, "onClick: " + mainFragment.isAdded());
+            }
+        });
+    }
+
+    private void changeFragment() {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.frame_layout, new MainFragment());
+        ft.replace(R.id.frame_layout, mainFragment);
         ft.commit();
     }
 }
