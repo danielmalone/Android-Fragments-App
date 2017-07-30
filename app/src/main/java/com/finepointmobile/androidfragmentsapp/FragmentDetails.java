fb5a40b0cb26e63b8231c330a710cfb18ab10af9
Daniel Malone <danieljmalone@gmail.com>
2017-07-29 19:31:56 -0500

    make layout orientation-sensitive

---
+++ app/src/main/java/com/finepointmobile/androidfragmentsapp/FragmentDetails.java
@@ -0,0 +1,22 @@
+package com.finepointmobile.androidfragmentsapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by danielmalone on 7/29/17.
+ */
+
+public class FragmentDetails extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.details, container, false);
+    }
+}
--- app/src/main/java/com/finepointmobile/androidfragmentsapp/MainActivity.java
+++ app/src/main/java/com/finepointmobile/androidfragmentsapp/MainActivity.java
@@ -5,14 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
     FloatingActionButton mFab;
-    FragmentHeadlines mainFragment;
     boolean mTwoPaneMode;
+    FrameLayout mHeadlines;
+    FrameLayout mDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +24,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        mainFragment = new FragmentHeadlines();
 
+        mHeadlines = (FrameLayout) findViewById(R.id.headlines);
+        mDetails = (FrameLayout) findViewById(R.id.details);
         mFab = (FloatingActionButton) findViewById(R.id.fab);
-
         mTwoPaneMode = getResources().getBoolean(R.bool.twoPaneMode);
+
+        if (mTwoPaneMode) {
+            mHeadlines.setLayoutParams(new LinearLayout.LayoutParams(3, ViewGroup.LayoutParams.MATCH_PARENT, 3));
+            mDetails.setLayoutParams(new LinearLayout.LayoutParams(0, 0, 0));
+        }
 
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.headlines, new FragmentHeadlines());
+        ft.replace(R.id.details, new FragmentDetails());
         ft.commit();
     }
 }
